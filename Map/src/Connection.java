0bbbf073af1586782356f517beb627ce6b92ab96
Kar1m_mttkn <89412432+kar44@users.noreply.github.com>
2022-11-10 21:19:26 +0100

    Delete Map directory

--- Map/src/Connection.java
+++
@@ -1,9 +0,0 @@
-public class Connection {
-    City city;
-    Integer distance;
-
-    public Connection(City name, Integer dist){
-        this.city = name;
-        this.distance = dist;
-    }
-}
--- Map/src/Paths.java
+++
@@ -1,9 +0,0 @@
-public class Paths {
-    City[] path;
-    int sp;
-
-    public Paths() {
-        path = new City[54];
-        sp = 0;
-    }
-}
