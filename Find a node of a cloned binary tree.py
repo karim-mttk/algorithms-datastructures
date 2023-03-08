6766e54dbd1493b6e1928e132eb6148cea3f9c4d
karim-mttk <muttakin.karim@gmail.com>
2023-03-08 12:45:20 +0100

    New commits

--- Find a node of a cloned binary tree.py
+++ Find a node of a cloned binary tree.py
@@ -5,9 +5,9 @@ class TreeNode:
         self.left = None
         self.right = None
 
-#v1. with iterative DFS
-class Solution:
-    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+#v1. with iterative DFS, O(n) time, O(n) space. 
+
+def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
         stack = [(original, cloned)]
 
         while stack:
@@ -23,3 +23,16 @@ class Solution:
 
         return None 
     
+ #v2. with recursive DFS, O(n) time, O(n) space.   
+def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+    def dfs(node1, node2):
+        if not node1:
+            return None
+        if node1 is target:
+            return node2
+        left_result = dfs(node1.left, node2.left)
+        if left_result:
+            return left_result
+        return dfs(node1.right, node2.right)
+    
+    return dfs(original, cloned)
