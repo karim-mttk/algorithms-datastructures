1ad0b3708798495127509a3716c491d31b21dff6
kar913 <muttakin.karim@gmail.com>
2023-04-12 10:41:01 +0200

    new commits

---
+++ two-pointers/two-sum-2.py
@@ -0,0 +1,15 @@
+# v1 using array swap
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        i = 0
+        j = n - 1
+        while i < j:
+            s = numbers[i] + numbers[j]
+            if s == target:
+                return [i+1, j+1]
+            elif s < target:
+                i += 1
+            else:
+                j -= 1
+        return []
