721c8d60812a06590cd7102d4a700b836e2014bd
karim-mttk <muttakin.karim@gmail.com>
2023-09-23 20:08:51 +0200

    new commits

--- sorting-algorithms/Insertion_sort.py
+++ sorting-algorithms/Insertion_sort.py
@@ -1,4 +1,6 @@
 # v1
+
+# insertion sort runtime = O(n^2)
 def insertion_sort(A):
     for j in range(1, len(A)):
         key = A[j]
@@ -8,6 +10,14 @@ def insertion_sort(A):
             i -= 1
         A[i + 1] = key
 
+#v2
+def sortArray(self, nums: List[int]) -> List[int]:
+    for i in range(1, len(nums)):  # Start from the second element (index 1)
+        j = i
+        while j > 0 and nums[j] < nums[j - 1]:
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]
+                j -= 1
+        return nums
 
 A = [5, 2, 4, 6, 1, 3]
 insertion_sort(A)
