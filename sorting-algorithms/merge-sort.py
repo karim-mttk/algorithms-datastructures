810d30491814ef898750bc60b8a67261d8bccf51
karim-mttk <muttakin.karim@gmail.com>
2023-09-24 19:21:12 +0200

    new commits

--- sorting-algorithms/merge-sort.py
+++ sorting-algorithms/merge-sort.py
@@ -37,3 +37,36 @@ a = [5, 1, 1, 2, 0, 0]
 sorted_arr = merge_sort(a)
 
 print(sorted_arr)
+
+
+def sortArray(self, nums: List[int]) -> List[int]:
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        L = nums[:mid]
+        R = nums[mid:]
+
+        self.sortArray(L)
+        self.sortArray(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                nums[k] = L[i]
+                i += 1
+            else:
+                nums[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            nums[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            nums[k] = R[j]
+            j += 1
+            k += 1
+
+    return nums
